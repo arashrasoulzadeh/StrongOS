be63fa331eb52d1c9bb22307b8b9f93623dc4c85
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2017-02-20 21:50:21 +0330

    set 255 as max task

--- kernel.h
+++ kernel.h
@@ -3,28 +3,20 @@ const int HIGH=2,MED=1,LOW=0;//speed of delays for each iteration
 
 class Task{
 public:
-  int id;
+  int id,counter,type;
   typedef void (*func)();
   func job;
-  int counter;
-  int type;
   Task(func _job,int _type=1)
   {
     job=_job;
     type=_type;
-    //tasks+=this;
   }
-  Task()
-  {
-
-  }
+  Task(){}
 };
 Task *tasks=new Task[0];
-int c=-1;
-int total=0;
+int c=-1,total=-1;
 
 class Strong{
-
 public:
   int lastid=-1;
   /*
@@ -40,12 +32,8 @@ public:
   */
   void tick()
   {
-    if (c>=total){
-      c=0;
-    }else{
-      c++;
-    }
-    //printf("running task %i\n",tasks[c].id);
+    c>=total ? c=0:c++;
+    //printf("running task %i\n",c);
     Task *t= &tasks[c];
     //if (t>0)
       t->job();
@@ -56,20 +44,16 @@ public:
   void scheduleJob(Task job)
   {
     lastid++;
-    Task *temp=new Task[lastid];
+    Task *temp=new Task[255];
     for (int i=0;i<lastid-1;i++)
     {
       temp[i]=tasks[i];
     }
     tasks=temp;
-
     tasks[lastid]=job;
     tasks[lastid].id=lastid;
     total++;
     delete [] temp;
   }
-  Strong()
-  {
-
-  }
+  Strong(){}
 };
--- run.cpp
+++ run.cpp
@@ -3,7 +3,9 @@
 Strong os;
 
 void hello(){printf("%s\n","hello world" );}
+void howudoing()  {printf("%s\n","how u doing" );}
 void bye()  {printf("%s\n","bye world" );}
+
 int main()
 {
   os.boot(HIGH);
@@ -14,10 +16,13 @@ int main()
   Task func_hello (hellofunc);
   os.scheduleJob(func_hello);
 
-//  Task::func byefunc = &bye;
-//  Task func_bye (byefunc);
-//  os.scheduleJob(func_bye);
+  Task::func howudoingfunc = &howudoing;
+  Task func_howudoing (howudoingfunc);
+  os.scheduleJob(func_howudoing);
 
+  Task::func byefunc = &bye;
+  Task func_bye (byefunc);
+  os.scheduleJob(func_bye);
 
 
 
